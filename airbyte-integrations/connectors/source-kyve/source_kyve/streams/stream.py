4cc1ac3e0a768bfbf3a9bfa3a053af60fe70203f
fabianriewe <fabian@kyve.network>
2023-01-11 16:00:52 +0100

    chore: testing dict check

--- airbyte-integrations/connectors/source-kyve/source_kyve/streams/stream.py
+++ airbyte-integrations/connectors/source-kyve/source_kyve/streams/stream.py
@@ -78,8 +78,14 @@ class KYVEStream(HttpStream, IncrementalMixin):
 
             # extract the value from the key -> value mapping
             for _ in decompressed_as_json:
-                r.append(_.get("value"))
+                v = self.parse_value(_.get("value"))
+                r.append(v)
         return r
+
+    def parse_value(self, value: Any) -> Mapping:
+        if isinstance(value, dict):
+            raise TypeError("Value from bundle is not a dict, use 'parse_value' to parse to dict.")
+        return value
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         json_response = response.json()
